313a678dc3cf132201acd054148f64f6e8bb8abd
teodosin <teodosin@protonmail.com>
2022-10-29 14:21:49 +0300

    Initialise DataAccess classes
    * Initialise DataAccess and DataAccessInMemory classes
    * Basic creation of new base nodes

--- main_graph_view/main_graph_view.gd
+++ main_graph_view/main_graph_view.gd
@@ -2,16 +2,27 @@ extends Node2D
 
 @onready var node_base = load("res://main_graph_view/nodes/node_base.tscn")
 @onready var wire_base = load("res://main_graph_view/wires/wire_base.tscn")
+
+var data_access: DataAccess = DataAccessInMemory.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _input(event):
 	if event.is_action_pressed("create_new_node"):
-		var newNode = node_base.instantiate()
-		newNode.set_position(get_global_mouse_position())
-		add_child(newNode)
+		createNode()
+		
+
+
+func createNode():
+	var newId = data_access.addNode()
+	
+	var newNode = node_base.instantiate()
+	newNode.set_position(get_global_mouse_position())
+	add_child(newNode)
+	
+	print(newId)
