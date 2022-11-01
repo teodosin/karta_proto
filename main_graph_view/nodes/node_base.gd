5d5a336398cdf5987729eb930892a139e256888e
Viktor <teodosin@protonmail.com>
2022-11-01 14:03:15 +0200

    Add positioning nodes by dragging

--- main_graph_view/nodes/node_base.gd
+++ main_graph_view/nodes/node_base.gd
@@ -3,12 +3,23 @@ extends Node2D
 
 var id: int
 
+var nodeMoving: bool = false
+var clickOffset: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$IdLabel.text = str(id)
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if nodeMoving:
+		self.set_position(get_global_mouse_position()-clickOffset)
+
+
+func _on_control_gui_input(event):
+	if event.is_action_pressed("mouseLeft"):
+		clickOffset = get_global_mouse_position() - self.position
+		nodeMoving = true
+	if event.is_action_released("mouseLeft"):
+		nodeMoving = false 
+	
