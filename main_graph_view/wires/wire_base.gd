5b5cd8b0a7d1c10c048fd78592ba523511221260
teodosin <teodosin@protonmail.com>
2022-11-01 22:31:46 +0200

    Create basic WireBase class
    * Add addWire basic function to data_access

--- main_graph_view/wires/wire_base.gd
+++ main_graph_view/wires/wire_base.gd
@@ -1,12 +1,4 @@
 class_name WireBase
-extends Line2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+var source: int
+var target: int
