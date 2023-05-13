6de672eab1a2e06cc78671700b6a41dff5fa100f
teodosin <teodosin@protonmail.com>
2023-05-13 10:59:16 +0300

    Forgot to save the files

--- main_graph_view/scene_layer.gd
+++ main_graph_view/scene_layer.gd
@@ -1,1 +1,49 @@
 extends CanvasLayer
+
+@onready var imageOutputTexture = $OutputTexture
+
+@onready var viewportContainer = $ViewportContainer
+@onready var viewport = $ViewportContainer/SubViewport
+
+enum OutputStates {
+	NONE,
+	IMAGE,
+	VIEWPORT
+}
+
+var activeOutput: OutputStates = OutputStates.NONE
+
+# Functions for changing the current output state
+func setOutputToImage():
+	activeOutput = OutputStates.IMAGE
+	
+	viewportContainer.visible = false
+	imageOutputTexture.visible = true
+	
+func setOutputToViewport():
+	activeOutput = OutputStates.VIEWPORT
+	
+	imageOutputTexture.visible = false
+	viewportContainer.visible = true
+	
+func setOutputToNone():
+	activeOutput = OutputStates.NONE
+	
+	imageOutputTexture.visible = false
+	viewportContainer.visible = false
+	
+func setOutputFromFocal(focal: NodeViewBase):
+	match focal.dataNode.nodeType:
+		"IMAGE":
+			setOutputToImage()
+		"OBJECT":
+			setOutputToViewport()
+		_:
+			setOutputToNone()
+		
+
+
+func addChildToViewport(child: Node):
+	viewport.add_child(child)	
+func removeChildFromViewport(child: Node):
+	viewport.remove_child(child)
